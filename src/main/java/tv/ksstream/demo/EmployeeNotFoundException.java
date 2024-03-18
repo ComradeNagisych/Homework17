50e510fc934cba882f5d1d4f44547c10a024704b
kiril <penzaringo2010@yandex.ru>
2024-03-18 23:54:42 +0300

    В процессе доработки

---
+++ src/main/java/tv/ksstream/demo/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package tv.ksstream.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException (String message) {
+        super(message);
+    }
+}
--- src/main/java/tv/ksstream/demo/service/EmployeeService.java
+++ src/main/java/tv/ksstream/demo/service/EmployeeService.java
@@ -1,5 +1,53 @@
 package tv.ksstream.demo.service;
+import org.springframework.stereotype.Service;
+import tv.ksstream.demo.Employee;
+import tv.ksstream.demo.EmployeeAlreadyAddedException;
+import tv.ksstream.demo.EmployeeNotFoundException;
+import tv.ksstream.demo.EmployeeStorageIsFullException;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
 
 public class EmployeeService {
-
+    private static final int maxEmployeeNumber = 50;
+    private final List<Employee> employees = new ArrayList<>(List.of(
+            new Employee("Alexey", "Alexeev"),
+            new Employee("Alexandr", "Alexandrov"),
+            new Employee("Alla", "Antonova"),
+            new Employee("Boris", "Borisov"),
+            new Employee("Victor", "Victorov"),
+            new Employee("Valentina", "Valentinova"),
+            new Employee("Maria", "Maximova"),
+            new Employee("Natalia", "Nikolaeva"),
+            new Employee("Sergey", "Sergeev"),
+            new Employee("Tatiana", "Timofeeva")
+    ));
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.size() >= maxEmployeeNumber) {
+            throw new EmployeeStorageIsFullException();
+        } else if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+        employees.add(employee);
+        return employee;
+    }
+    public Employee deleteEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            employees.remove(employee);
+        } else {
+            throw new EmployeeNotFoundException();
+        }
+        return employee;
+    }
+    public void findEmployee (String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            System.out.println("Сотрудник " + firstName + " " + lastName + " найден");
+        } else {
+            throw new EmployeeNotFoundException();
+        }
+    }
 }
